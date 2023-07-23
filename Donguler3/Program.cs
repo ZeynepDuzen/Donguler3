f0ae7004b8556a197b2893807752df92abe5978b
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-23 11:20:43 +0300

    Proje dosyası ekle.

---
+++ Donguler3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for dongusu ile klavyeden girilen sayinin tam bolenlerini bulma
+            int sayi;
+            int i;
+            Console.WriteLine("Lutfen bir sayi giriniz : ");
+            Console.WriteLine();
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (i = 1; i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
